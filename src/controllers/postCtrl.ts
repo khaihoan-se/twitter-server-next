96d28f24389c877123d668a2e4861e46cd7721b1
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-08-18 23:05:07 +0700

    🥩🥩 post 🥩🥩

--- src/controllers/postCtrl.ts
+++ src/controllers/postCtrl.ts
@@ -3,6 +3,23 @@ import { Response } from 'express'
 import { IPost } from '../types'
 import { fileSizeFormatter } from '../utils'
 
+class APIfeatures {
+    query: any;
+    queryString: any;
+    constructor(query: any, queryString: any) {
+        this.query = query
+        this.queryString = queryString
+    }
+
+    paginating() {
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 5
+        const skip = (page - 1) * limit
+        this.query = this.query.skip(skip).limit(limit)
+        return this
+    }
+}
+
 const postCtrl = {
     createPost: async (req: IPost, res: Response) => {
         try {
@@ -67,6 +84,23 @@ const postCtrl = {
                     user: req.user
                 }
             })
+        } catch (err: any) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    getPosts: async (req: any, res: Response) => {
+        try {
+            const features =  new APIfeatures(Posts.find(), req.query).paginating()
+
+            const posts = await features.query.sort('-createdAt')
+            .populate("user likes", "avatar username fullname followers")
+
+            res.json({
+                msg: 'Success!',
+                result: posts.length,
+                posts
+            })
+
         } catch (err: any) {
             return res.status(500).json({msg: err.message})
         }
--- src/router/postRouter.ts
+++ src/router/postRouter.ts
@@ -4,7 +4,9 @@ import postCtrl from '../controllers/postCtrl'
 import upload from '../middleware/upload'
 
 const router = express.Router()
-
+// @route   POST api/create-post
 router.post('/create-post', upload.array('images'), auth, postCtrl.createPost)
+// @route   GET api/get-posts
+router.get('/get-posts', auth, postCtrl.getPosts)
 
 export default router
